616829332f917013d38b48f279e7369459ec1b34
misaka <1784321894@qq.com>
2022-07-13 21:19:44 +0800

    Mybatis Demo1 : auto commit

--- MyBatis-Demo1/src/test/java/com/misaka/test/MybatisTest.java
+++ MyBatis-Demo1/src/test/java/com/misaka/test/MybatisTest.java
@@ -19,13 +19,13 @@ public class MybatisTest {
         // 3.创建SqlSessionFactory
         SqlSessionFactory sqlSessionFactory = builder.build(resourceAsStream);
         // 4.创建SqlSession
-        SqlSession sqlSession = sqlSessionFactory.openSession();
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
         // 5.创建UserMapper接口的实现类对象
         UserMapper mapper = sqlSession.getMapper(UserMapper.class);
         // 6.执行SQL语句
         int result = mapper.insertUser();
         // 7.提交事务
-        sqlSession.commit();
+//        sqlSession.commit();
 
         System.out.println("result = " + result);
 
