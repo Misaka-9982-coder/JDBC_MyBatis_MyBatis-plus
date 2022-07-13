2e88ea3500e2932b15706698ff26173b51efccdb
misaka <1784321894@qq.com>
2022-07-13 22:11:34 +0800

    Mybatis Demo1 : update, delete, query

--- MyBatis-Demo1/src/main/java/com/misaka/mapper/UserMapper.java
+++ MyBatis-Demo1/src/main/java/com/misaka/mapper/UserMapper.java
@@ -1,6 +1,23 @@
 package com.misaka.mapper;
 
+import com.misaka.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     // 添加用户信息
     int insertUser();
+
+    // 更新
+    void updateUser();
+
+    void deleteUser();
+
+
+    // 根据id查询用户信息
+    User getUserById();
+
+
+    // 查询所有的用户信息
+    List<User> getAllUser();
 }
--- MyBatis-Demo1/src/test/java/com/misaka/test/MybatisTest.java
+++ MyBatis-Demo1/src/test/java/com/misaka/test/MybatisTest.java
@@ -1,13 +1,16 @@
 package com.misaka.test;
 
 import com.misaka.mapper.UserMapper;
+import com.misaka.pojo.User;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class MybatisTest {
     @Test
@@ -29,5 +32,35 @@ public class MybatisTest {
 
         System.out.println("result = " + result);
 
+    }
+
+    @Test
+    public void updateUser() throws Exception {
+        // 1.读取配置文件
+        InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+        // 2.创建SqlSessionFactoryBuilder
+        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
+        // 3.创建SqlSessionFactory
+        SqlSessionFactory sqlSessionFactory = builder.build(resourceAsStream);
+        // 4.创建SqlSession
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        // 5.创建UserMapper接口的实现类对象
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        // 6.执行SQL语句
+        mapper.updateUser();
+    }
+
+    @Test
+    public void testCRUD() throws IOException {
+        InputStream is = Resources.getResourceAsStream("mybatis-config.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(is);
+        SqlSession sqlSession = sqlSessionFactory.openSession(true);
+        UserMapper mapper = sqlSession.getMapper(UserMapper.class);
+        //mapper.updateUser();
+        //mapper.deleteUser();
+        /*User user = mapper.getUserById();
+        System.out.println(user);*/
+        List<User> list = mapper.getAllUser();
+        list.forEach(user -> System.out.println(user));
     }
 }
