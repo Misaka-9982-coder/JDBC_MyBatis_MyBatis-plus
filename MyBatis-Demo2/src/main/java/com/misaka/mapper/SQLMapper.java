4b4b84c213ddcd53ecb58e8a86fc62c5676a216a
misaka <1784321894@qq.com>
2022-07-14 14:39:53 +0800

    Mybatis Demo2 : fuzzy query

---
+++ MyBatis-Demo2/src/main/java/com/misaka/mapper/SQLMapper.java
@@ -0,0 +1,11 @@
+package com.misaka.mapper;
+
+import com.misaka.pojo.User;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface SQLMapper {
+
+    List<User> getUserByLike(@Param("username") String username);
+}
---
+++ MyBatis-Demo2/src/test/java/com/misaka/SQLMapperTest.java
@@ -0,0 +1,23 @@
+package com.misaka;
+
+import com.misaka.mapper.SQLMapper;
+import com.misaka.pojo.User;
+import com.misaka.utils.SqlSessionUtils;
+import org.apache.ibatis.session.SqlSession;
+import org.junit.Test;
+
+import java.util.List;
+
+public class SQLMapperTest {
+
+    @Test
+    public void testGetUserByLike() {
+        SqlSession sqlSession = SqlSessionUtils.getSqlSession();
+        SQLMapper mapper = sqlSession.getMapper(SQLMapper.class);
+        List<User> usersByLike = mapper.getUserByLike("张三");
+        for (User user : usersByLike) {
+            System.out.println(user);
+        }
+    }
+}
+
