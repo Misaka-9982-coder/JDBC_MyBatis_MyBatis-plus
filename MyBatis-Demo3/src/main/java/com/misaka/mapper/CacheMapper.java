442b3b407c437637571a9a8704507e685a410ca0
misaka <1784321894@qq.com>
2022-07-15 11:51:24 +0800

    Mybatis Demo3 : Invaliad the first grade cache

--- MyBatis-Demo3/src/main/java/com/misaka/mapper/CacheMapper.java
+++ MyBatis-Demo3/src/main/java/com/misaka/mapper/CacheMapper.java
@@ -6,4 +6,6 @@ import org.apache.ibatis.annotations.Param;
 public interface CacheMapper {
 
     Emp getEmpById(@Param("eid") Integer eid);
+
+    void insertEmp(Emp emp);
 }
--- MyBatis-Demo3/src/test/java/com/misaka/mybatis/test/CacheMapperTest.java
+++ MyBatis-Demo3/src/test/java/com/misaka/mybatis/test/CacheMapperTest.java
@@ -23,4 +23,45 @@ public class CacheMapperTest {
         Emp emp2 = mapper2.getEmpById(1);
         System.out.println(emp2);
     }
+
+    @Test
+    public void testCache1() {
+        SqlSession sqlSession1 = SqlSessionUtils.getSqlSession();
+        CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
+        Emp emp1 = mapper1.getEmpById(1);
+        System.out.println(emp1);
+
+        System.out.println("==============================");
+
+        mapper1.insertEmp(new Emp(null, "test", 12, "男", "123@qq.com"));
+
+        System.out.println("==============================");
+
+//        SqlSession sqlSession2 = SqlSessionUtils.getSqlSession();
+//        CacheMapper mapper2 = sqlSession1.getMapper(CacheMapper.class);
+        CacheMapper mapper2 = sqlSession1.getMapper(CacheMapper.class);
+        Emp emp2 = mapper2.getEmpById(1);
+        System.out.println(emp2);
+    }
+
+    @Test
+    public void testCache2() {
+        SqlSession sqlSession1 = SqlSessionUtils.getSqlSession();
+        CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
+        Emp emp1 = mapper1.getEmpById(1);
+        System.out.println(emp1);
+
+        System.out.println("==============================");
+
+//        mapper1.insertEmp(new Emp(null, "test", 12, "男", "123@qq.com"));
+        sqlSession1.clearCache();
+
+        System.out.println("==============================");
+
+//        SqlSession sqlSession2 = SqlSessionUtils.getSqlSession();
+//        CacheMapper mapper2 = sqlSession1.getMapper(CacheMapper.class);
+        CacheMapper mapper2 = sqlSession1.getMapper(CacheMapper.class);
+        Emp emp2 = mapper2.getEmpById(1);
+        System.out.println(emp2);
+    }
 }
