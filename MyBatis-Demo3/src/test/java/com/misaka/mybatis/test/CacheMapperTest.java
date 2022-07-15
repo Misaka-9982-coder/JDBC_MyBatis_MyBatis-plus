c39e383207a8a24577db8d8f1bb18348cc6848ed
misaka <1784321894@qq.com>
2022-07-15 14:18:50 +0800

    Mybatis Demo3 : the second grade cache

--- MyBatis-Demo3/src/test/java/com/misaka/mybatis/test/CacheMapperTest.java
+++ MyBatis-Demo3/src/test/java/com/misaka/mybatis/test/CacheMapperTest.java
@@ -3,8 +3,13 @@ package com.misaka.mybatis.test;
 import com.misaka.mapper.CacheMapper;
 import com.misaka.pojo.Emp;
 import com.misaka.utils.SqlSessionUtils;
+import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.Test;
+
+import java.io.InputStream;
 
 public class CacheMapperTest {
 
@@ -63,5 +68,27 @@ public class CacheMapperTest {
         CacheMapper mapper2 = sqlSession1.getMapper(CacheMapper.class);
         Emp emp2 = mapper2.getEmpById(1);
         System.out.println(emp2);
+    }
+
+    @Test
+    public void testSecondCache() {
+        try {
+            InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
+            SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+            SqlSession sqlSession1 = sqlSessionFactory.openSession();
+            CacheMapper mapper1 = sqlSession1.getMapper(CacheMapper.class);
+            System.out.println(mapper1.getEmpById(1));
+            sqlSession1.close();
+
+            System.out.println("==============================");
+
+            SqlSession sqlSession2 = sqlSessionFactory.openSession();
+            CacheMapper mapper2 = sqlSession2.getMapper(CacheMapper.class);
+            System.out.println(mapper2.getEmpById(1));
+            sqlSession2.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
